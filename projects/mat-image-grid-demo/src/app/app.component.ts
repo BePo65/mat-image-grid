5dac0898333e4b31c3b7963a7391a659479e0a56
Bernhard Pottler <bpottler@nexgo.de>
2024-04-26 06:48:46 +0200

    fix(demo): update active tab ehrn opening a tab via url

--- projects/mat-image-grid-demo/src/app/app.component.ts
+++ projects/mat-image-grid-demo/src/app/app.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
-import { RouterModule } from '@angular/router';
+import {
+  NavigationEnd,
+  Router,
+  RouterOutlet,
+  RouterLink,
+} from '@angular/router';
 
 import { MatImageGridLibComponent } from 'projects/mat-image-grid-lib/src';
 
@@ -9,12 +14,12 @@ type RouteTab = { title: string; route: string; index: number };
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatImageGridLibComponent, MatTabsModule, RouterModule],
+  imports: [MatImageGridLibComponent, MatTabsModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'Mat-Image-Grid-Demo';
 
   protected tabs: RouteTab[] = [
@@ -23,4 +28,17 @@ export class AppComponent {
     { title: 'Large Dataset', route: '/large-dataset', index: 2 },
   ];
   protected activeTab = this.tabs[0].index;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const activeRoute = this.tabs.find(
+          (routeDefinition) => routeDefinition.route === event.url,
+        );
+        this.activeTab = activeRoute?.index ?? -1;
+      }
+    });
+  }
 }
